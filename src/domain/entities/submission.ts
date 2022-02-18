0872062d27603809e4a2158dae11f1a473395d77
Gabriel Borges <gbdesigns13@gmail.com>
2022-02-17 22:51:20 -0300

    Projeto final

---
+++ src/domain/entities/submission.ts
@@ -0,0 +1,25 @@
+import { Entity } from "../../core/domain/Entity";
+
+type SubmissionProps = {
+  challengeId: string;
+  studentId: string;
+  createdAt?: Date;
+};
+
+export class Submission extends Entity<SubmissionProps> {
+  private constructor(props: SubmissionProps, id?: string) {
+    super(props, id);
+  }
+
+  static create(props: SubmissionProps, id?: string) {
+    const correction = new Submission(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id
+    );
+
+    return correction;
+  }
+}
